26706d61141d5e402f104193aa5a97ca38cc3d1d
Dararat Bishop <dararat246@gmail.com>
2023-08-18 23:08:42 -0400

    Original prorject profile

---
+++ app.js
@@ -0,0 +1,37 @@
+// require("dotenv").config();
+
+const express = require("express");
+
+const mongoose = require("mongoose");
+const { errors } = require("celebrate");
+
+const cors = require("cors");
+const helmet = require("helmet");
+
+// const { errorHandler } = require("./middlewares/error-handler");
+// const { requestLogger, errorLogger } = require("./middlewares/logger");
+
+const limiter = require("./utils/limiter");
+
+const { PORT = 3001 } = process.env;
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+app.use(cors());
+
+// app.use(requestLogger);
+
+app.use(routes);
+// app.use(errorLogger);
+
+app.use(errors());
+// app.use(errorHandler);
+
+app.listen(PORT);
---
+++ controllers/users.js
@@ -0,0 +1,131 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const BadRequestError = require("../errors/BadRequestError");
+const ConFlictError = require("../errors/ConflictError");
+const NotFoundError = require("../errors/NotFoundError");
+const UnauthorizedError = require("../errors/UnauthorizedError");
+const { JWT_SECRET } = require("../utils/config");
+
+const getUsers = (req, res, next) => {
+  User.find({})
+    .then((users) => {
+      if (!users) {
+        next(new NotFoundError("User not found"));
+      } else {
+        res.send({ data: users });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const getUser = (req, res, next) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        next(new NotFoundError("User not found"));
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        next(new BadRequestError("Invalid user ID"));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const createUser = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!password) {
+    next(new BadRequestError("Password is required"));
+  }
+  return bcrypt
+    .hash(password, 10)
+    .then((hash) =>
+      User.create({
+        email,
+        name,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      res.send({
+        name: user.name,
+        _id: user._id,
+        email: user.email,
+      });
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        next(new BadRequestError("Invalid data provided"));
+      } else if (err.code === 11000) {
+        next(
+          new ConFlictError(
+            "This email address is already used with an account"
+          )
+        );
+      } else {
+        next(err);
+      }
+    });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      if (!user) {
+        return next(new UnauthorizedError("Email or Password not found"));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          next(new UnauthorizedError("Email or Password not found"));
+        }
+        const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+          expiresIn: "7d",
+        });
+        res.send({ token });
+      });
+    })
+    .catch(() => {
+      next(
+        new UnauthorizedError(
+          "Login is denies due to invalid email or password"
+        )
+      );
+    });
+};
+
+const getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        next(new NotFoundError("User not found"));
+      } else {
+        res.send({ data: user });
+      }
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        next(new BadRequestError("Invalid user ID"));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  login,
+  getCurrentUser,
+};
